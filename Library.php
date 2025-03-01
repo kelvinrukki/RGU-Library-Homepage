5ae96add7e35cbaef18863bf6d94f7b260ecdba7
kelvinrukki <kelvinrukki@gmail.com>
2025-03-01 17:11:12 +0000

    Create Library.php

---
+++ Library.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>RGU Library System</title>
+        <link rel = "stylesheet" href = "CSS/bootstrap.css">
+    </head>
+    <body>
+        <header>
+            <h1>RGU Library System</h1>
+            <nav class="navbar navbar-expand-sm bg-light justify-content-center">
+                <div class="container-fluid">
+                    <a href ="#">Home page</a></li>
+                    <a href ="#">Login</a></li>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <section>
+                <h2>Welcome to Rgu Library</h2>
+                <form>
+                    <input type="text" placeholder="Search">
+                    <input type="Submit" value="Search">
+                </form>
+            </section>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
